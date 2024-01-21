4ee09ce418fd650a3d4b0c94ed5a54c8cc27c84a
Wilmer Arambula <terabytesoftw@gmail.com>
2024-01-21 08:09:32 -0300

    Clean `composer.json`, update `LICENSE`.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -64,8 +64,8 @@ class TestCase extends \PHPUnit\Framework\TestCase
             'vendorPath' => dirname(__DIR__) . '/vendor',
             'language' => 'en-US',
             'aliases' => [
-                '@bower' => dirname(__DIR__) . '/node_modules',
-                '@npm' => dirname(__DIR__) . '/node_modules',
+                '@bower' => '@vendor/bower-asset',
+                '@npm' => '@vendor/npm-asset',
             ],
             'components' => [
                 'i18n' => [
